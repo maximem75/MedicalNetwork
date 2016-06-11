d21da7d6da0f56f2f972fa546135f4ca1e119ab0
Hydeo <p.garciapelayo@gmail.com>
2016-06-11 19:49:17 +0200

    Messages
    Possibilité d'échanger des messages en temps réel

--- src/main/resources/server.js
+++ src/main/resources/server.js
@@ -8,6 +8,9 @@ httpServer = http.createServer(function(req, res){
 httpServer.listen(1337);
 var io = require('socket.io').listen(httpServer);
 var users = {}
+var messages = []
+var history = 2; // nombre de message historique à charger
+
 io.sockets.on('connection', function(socket){
 	
 	var me=false;
@@ -17,9 +20,30 @@ io.sockets.on('connection', function(socket){
 		socket.emit('newusr',users[k]);
 	}
 
+	for(var k in messages){
+		socket.emit('newmsg',messages[k]);
+	}
+	// ****
+	//  Reception  message
+	// ****
+
+	socket.on('newmsg',function(message){
+		message.user = me;
+		date = new Date();
+		message.h = date.getHours();
+		message.m = date.getMinutes();
+		messages.push(message);           // A faire fonctionner avec bdd
+		if(messages.length > history){
+			messages.shift() // supprime l'entrée la plus veille
+		}
+		io.sockets.emit('newmsg',message);
+	});
 
 
-	// User se connecte
+	// ****
+	//  User se connecte
+	// ****
+
 	socket.on('login', function(user){
 		me = user;
 		me.id = user.mail.replace('@','-').replace('.','_');
@@ -28,15 +52,18 @@ io.sockets.on('connection', function(socket){
 		socket.emit('logged');
 		users[me.id] = me;
 		io.sockets.emit('newusr',me);
-	})
+	});
 
-	//User se deconnecte
+	// ****
+	//  User se deconnecte
+	// ****
+
 	socket.on('disconnect', function(){
 		if(!me){
 			return false;
 		}
 		delete users[me.id];
 		io.sockets.emit('disusr',me);
-	})
+	});
 
 });
--- src/main/resources/static/js/client.js
+++ src/main/resources/static/js/client.js
@@ -1,25 +1,53 @@
 (function($){
 
 	var socket = io.connect('http://localhost:1337');
-
+	// On sauvegarde le bloc HTML 'msgtpl' et on supprime le spécimen qu'on réinjectera avec des valeurs dans l'event 'newusr'
+	var msgtpl = $('#msgtpl').html();
+	
+	$('#msgtpl').remove();
 	$('#loginform').submit(function(event){
 		event.preventDefault();
 		socket.emit('login',{
 			username: $('#username').val(),
 			mail	: $('#mail').val()
 		})
-	})
+	});
 
 	socket.on('logged', function(){
 		$('#login').fadeOut();
-	})
+		$('#message').focus();
+	});
+
+	// ****
+	//  Envoi message
+	// ****
+
+	$('#form').submit(function(event){
+		event.preventDefault();
+		socket.emit('newmsg', {message: $('#message').val()});
+		$('#message').val('');
+		$('#message').focus();
+	});
+
+	// ****
+	//  Reception message message
+	// ****
+	socket.on('newmsg', function(message){
+		// Injection d'un message
+		$('#messages').append('<div class="message">' + Mustache.render(msgtpl, message) + '</div>');
+		$('#messages').animate({scrollTop : $('#messages').prop('scrollHeight') }, 500); // Permet d'auto scroll a la reception d'un message, BEAUCOUP plus agréable
+	});
+
+	// ****
+	//Gestion des connctés
+	// ****
 
 	socket.on('newusr',function(user){
 		$('#users').append('<img src="' + user.avatar + '" id="' + user.id + '">');
-	})
+	});
 
 	socket.on('disusr',function(user){
 		$('#'+user.id).remove();
-	})
+	});
 
 })(jQuery);
