b8f8638f2b9d9233fefe9e0be4350c240b7b41fa
Yohan <yohanlamerand@gmail.com>
2016-06-26 16:17:58 +0200

    TU Category Controller suite

--- src/test/java/com/esgi/controllers/CategoryControllerTests.java
+++ src/test/java/com/esgi/controllers/CategoryControllerTests.java
@@ -1,6 +1,5 @@
 package com.esgi.controllers;
 
-import com.esgi.model.Category;
 import com.esgi.repositories.CategoryRepository;
 import com.esgi.repositories.UserRepository;
 import org.junit.Test;
@@ -9,16 +8,18 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Date;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Yohan on 22/06/2016.
  */
 @RunWith(MockitoJUnitRunner.class)
 public class CategoryControllerTests {
-
-    @Mock
-    private Category category;
 
     @Mock
     private CategoryRepository categoryRepository;
@@ -32,9 +33,27 @@ public class CategoryControllerTests {
     @Test
     public void should_getCategory()
     {
-        category.setIdcategory(1L);
-        category.setNameCategory("Doctor");
-        categoryController.getCategory("Doctor",1L);
+        when(userRepository.findByToken(eq("toto"), any(Date.class))).thenReturn(2L);
+        categoryController.getCategory("toto",1L);
+        verify(userRepository).findByToken(eq("toto"),any(Date.class));
         verify(categoryRepository).findOne(1L);
+    }
+
+    @Test
+    public void should_getCategoryByResearch()
+    {
+        when(userRepository.findByToken(eq("titi"), any(Date.class))).thenReturn(2L);
+        categoryController.getCategoryByResearch("titi","research");
+        verify(userRepository).findByToken(eq("titi"),any(Date.class));
+        verify(categoryRepository).getCategoriesByResearch(eq("research"));
+    }
+
+    @Test
+    public void should_getCategoriesList()
+    {
+        when(userRepository.findByToken(eq("tutu"), any(Date.class))).thenReturn(2L);
+        categoryController.getCategoriesList("tutu");
+        verify(userRepository).findByToken(eq("tutu"),any(Date.class));
+        verify(categoryRepository).findAll();
     }
 }
--- src/test/java/com/esgi/controllers/UserControllerTests.java
+++ src/test/java/com/esgi/controllers/UserControllerTests.java
@@ -1,13 +1,19 @@
 package com.esgi.controllers;
 
+import com.esgi.model.Category;
 import com.esgi.model.User;
 import com.esgi.repositories.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Date;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
 
 /**
@@ -20,16 +26,86 @@ public class UserControllerTests {
     private UserController userController;
 
     @Mock
-    private User user;
-
-    @Mock
     private UserRepository userRepository;
 
     @Test
-    public void should_register_user() {
-        user.setName("FlaeschTest");
-        user.setFirstname("ArnaudTest");
-        userController.registration(user);
-        verify(userRepository).save(user);
+    public void should_getUser()
+    {
+        Mockito.when(userRepository.findByToken(eq("toto") , any(Date.class))).thenReturn(2L);
+        userController.getUser("toto");
+        verify(userRepository).findByToken(eq("toto"),any(Date.class));
+        verify(userRepository).findOne(2L);
+    }
+
+    @Test
+    public void should_getUsersByCategory()
+    {
+        Mockito.when(userRepository.findByToken(eq("toto") , any(Date.class))).thenReturn(2L);
+        userController.getUsersByCategory("toto",1L);
+        verify(userRepository).findByToken(eq("toto"),any(Date.class));
+        verify(userRepository).findUsersByCategory(any(Category.class));
+    }
+
+    @Test
+    public void should_login()
+    {
+        Mockito.when(userRepository.findByLoginAndPassword(eq("login"),eq("password"))).thenReturn(new User());
+        userController.login("login","password");
+        verify(userRepository).findByLoginAndPassword(eq("login"),eq("password"));
+        verify(userRepository).save(any(User.class));
+    }
+
+    @Test
+    public void should_logout() {
+
+        Mockito.when(userRepository.findByToken(eq("titi"),any(Date.class))).thenReturn(1L);
+        Mockito.when(userRepository.findOne(1L)).thenReturn(new User());
+        userController.logout("titi");
+        verify(userRepository).findByToken(eq("titi"),any(Date.class));
+        verify(userRepository).findOne(1L);
+        verify(userRepository).save(any(User.class));
+    }
+
+    @Test
+    public void should_registration()
+    {
+        userController.registration(new User());
+        verify(userRepository).save(any(User.class));
+    }
+
+    @Test
+    public void should_updateData()
+    {
+        Mockito.when(userRepository.findByToken(eq("token"),any(Date.class))).thenReturn(1L);
+        userController.updateData(new User(),"token");
+        verify(userRepository).findByToken(eq("token"),any(Date.class));
+        verify(userRepository).save(any(User.class));
+    }
+
+    @Test
+    public void should_deleteUser()
+    {
+        Mockito.when(userRepository.findByToken(eq("token"),any(Date.class))).thenReturn(1L);
+        userController.deleteUser("token");
+        verify(userRepository).findByToken(eq("token"),any(Date.class));
+        verify(userRepository).delete(1L);
+    }
+
+    @Test
+    public void getInvitations()
+    {
+        Mockito.when(userRepository.findByToken(eq("token"),any(Date.class))).thenReturn(1L);
+        userController.getInvitations("token");
+        verify(userRepository).findByToken(eq("token"),any(Date.class));
+        verify(userRepository).findPendingInvitations(1l, false);
+    }
+
+    @Test
+    public void should_getContacts()
+    {
+        Mockito.when(userRepository.findByToken(eq("token"),any(Date.class))).thenReturn(1L);
+        userController.getContacts("token");
+        verify(userRepository).findByToken(eq("token"),any(Date.class));
+        verify(userRepository).findContacts(1L,true);
     }
 }
