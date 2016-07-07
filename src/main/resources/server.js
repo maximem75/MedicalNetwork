83ff6aa7da387d95848f1315d3e340bca9639d51
hideo <p.garciapelayo@gmail.com>
2016-07-07 20:49:35 +0200

    Add message et get ok

--- src/main/resources/server.js
+++ src/main/resources/server.js
@@ -16,32 +16,55 @@ var history = 2; // nombre de message historique à charger
 //*****
 //-------------- REST CALL ------------------
 //*****
-rest.get('http://demo3723079.mockable.io/medicalNetworkAddMessage').on('complete', function(result) {
+/*rest.get('http://localhost:8080/message/getConversation?token=6f163583-b904-4d20-943c-ed24c2872e00&idcontact=2').on('complete', function(result) {
   if (result instanceof Error) {
     console.log('Error:', result.message);
     this.retry(5000); // try again after 5 sec 
   } else {
     console.log(result);
   }
-});
+});*/
 
-rest.post(' http://demo3723079.mockable.io/MedicalNetworkPost', {
-  data: { nom: "nom test" },
-}).on('complete', function(data, response) {
-    console.log("Post succed ? "+response.raw);
-  
-}).on('fail',function(data,response){
-	console.log("faiiiiiiiiil");
-});
+
+var jsonData =  { 
+	  	date : "2012-03-02", 
+	  	content : "blabla", 
+	  	sender : {"iduser" : "1"}, 
+	  	receiver :{"iduser" : "2"}  
+};
+rest.postJson('http://localhost:8080/message/addMessage?token=6f163583-b904-4d20-943c-ed24c2872e00', jsonData).on('complete', function(data, response) {
+	    console.log("Post succed ? "+response.raw);
+	}).on('fail',function(data,response){
+		console.log("faiiiiiiiiil");
+	});
+
+	/*rest.post('http://localhost:8080/message/addMessage?token=6f163583-b904-4d20-943c-ed24c2872e00', {
+	  headers: { 
+	        'Accept': 'application/json',
+	        'Content-Type': 'application/json' 
+	  },
+	  data: { 
+	  	date : "12/03/2012", 
+	  	content : "blabla", 
+	  	sender : {"iduser" : "1"}, 
+	  	receiver :{"iduser" : "2"}  
+
+	  },
+	}).on('complete', function(data, response) {
+	    console.log("Post succed ? "+response.raw);
+	}).on('fail',function(data,response){
+		console.log("faiiiiiiiiil");
+	});*/
 //------------------------------------------
 
 io.sockets.on('connection', function(socket){
 	
 	var me=false;
 
-	for(var k in messages){
+
+	/*for(var k in messages){
 		socket.emit('newmsg',messages[k]);
-	}
+	}*/
 
 	// ****
 	//  Reception  message
@@ -68,6 +91,26 @@ io.sockets.on('connection', function(socket){
 		me.id = user.mail.replace('@','-').replace('.','_');
 		me.avatar = 'http://forum.canardpc.com/customavatars/thumbs/avatar16737_1.gif';
 		me.room = user.room
+
+		rest.get('http://localhost:8080/message/getConversation?token=6f163583-b904-4d20-943c-ed24c2872e00&idcontact=2').on('complete', function(result) {
+		  if (result instanceof Error) {
+		    console.log('Error:', result.message);
+		    this.retry(5000); // try again after 5 sec 
+		  } else {
+		    console.log(result);
+		    var message = [];
+		    for(var bddMessage in result){
+		    date = new Date();
+			message.h = date.getHours();
+			message.m = date.getMinutes();
+
+		    message.user = me;
+		    message.message = bddMessage[1];
+			socket.emit('newmsg',message);
+		}
+		  }
+		});
+
 
 		for(var k in users){
 			if(users[k].room == me.room)
